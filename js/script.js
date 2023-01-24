9d253b5c30f6395ae1acaaac6e82a5890630ea6b
vijoh01 <vijoh01@gmail.com>
2023-01-24 14:26:34 +0100

    Löste lite prestandaproblem och lagt till logga ut funktion

--- js/script.js
+++ js/script.js
@@ -1,17 +1,12 @@
-// Import the functions you need from the SDKs you need
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-app.js";
-
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-auth.js";
-
-// TODO: Add SDKs for Firebase products that you want to use
+import { getAuth, signOut, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-auth.js";
 import {
-  getDatabase,
-  ref,
-  set,
-  onValue,
-  remove,
-  push,
+    getDatabase,
+    ref,
+    set,
+    onValue,
+    remove,
+    push,
 } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-database.js";
 
 const loginWrapper = document.querySelector('#loginWrapper');
@@ -19,21 +14,23 @@ const loginWrapper = document.querySelector('#loginWrapper');
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  databaseURL:
-    "https://chatapp-76264-default-rtdb.europe-west1.firebasedatabase.app/",
+    databaseURL:
+        "https://chatapp-76264-default-rtdb.europe-west1.firebasedatabase.app/",
 
-  apiKey: "AIzaSyAZ0KJhUc6ltrF7QACjM8IW2JqeLWT3n3g",
+    apiKey: "AIzaSyAZ0KJhUc6ltrF7QACjM8IW2JqeLWT3n3g",
 
-  authDomain: "chatapp-76264.firebaseapp.com",
+    authDomain: "chatapp-76264.firebaseapp.com",
 
-  projectId: "chatapp-76264",
+    projectId: "chatapp-76264",
 
-  storageBucket: "chatapp-76264.appspot.com",
+    storageBucket: "chatapp-76264.appspot.com",
 
-  messagingSenderId: "957844540887",
+    messagingSenderId: "957844540887",
 
-  appId: "1:957844540887:web:b31870f3a4d046dd3d4f92",
+    appId: "1:957844540887:web:b31870f3a4d046dd3d4f92",
 };
+
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -44,10 +41,10 @@ console.log(database);
 
 // skriva
 function writeUserData(message) {
-  let adressRef = ref(database, "user");
-  set(adressRef, {
-    namn: message,
-  });
+    let adressRef = ref(database, "user");
+    set(adressRef, {
+        namn: message,
+    });
 }
 
 writeUserData("SUPP DOG!!!");
@@ -55,45 +52,59 @@ writeUserData("SUPP DOG!!!");
 // läsa en specifik onValue direkt + när den ändrar value
 const urlRef = ref(database, "user");
 onValue(urlRef, (snapshot) => {
-  const data = snapshot.val();
-  console.log(data.namn);
+    const data = snapshot.val();
+    console.log(data.namn);
 });
 
 var loginForm = document.getElementById("login-form");
 var emailInput = document.getElementById("email");
 var passwordInput = document.getElementById("password");
+var logout = document.getElementById("logout");
+
+
 
 loginForm.addEventListener("click", function (e) {
-  e.preventDefault();
-  var email = emailInput.value;
-  var password = passwordInput.value;
-  //register();
-  if (e.target.id == "login-button") login(email, password);
-  else if (e.target.id == "register-button") register();
+    e.preventDefault();
+    var email = emailInput.value;
+    var password = passwordInput.value;
+    //register();
+    if (e.target.id == "login-button") login(email, password);
+    else if (e.target.id == "register-button") register(email, password);
 });
 
-const auth = getAuth();
 
+logout.addEventListener("click", function (e) {
+    signOut(auth).then(() => {
+        console.log("logout");
+        loginWrapper.classList.add('hide');
+    }).catch((error) => {
+        // An error happened.
+    });
+})
+
+const auth = getAuth();
+let currentUser = auth.currentUser;
 
 onAuthStateChanged(auth, (user) => {
-  if (user) {
-    console.log('user already signed in')
-    loginWrapper.classList.add('hide');
-    const uid = user.uid;
-  } else {
-    // User is signed out
-    // ...
-  }
+    if (user) {
+        console.log('user already signed in')
+        loginWrapper.classList.remove('hide');
+        const uid = user.uid;
+    } else {
+        // User is signed out
+        // ...
+    }
 });
 
 function login(email, password) {
     signInWithEmailAndPassword(auth, email, password)
         .then(function (user) {
             // Login success
+        
             console.log(user);
             alert(user.user.email + " has signed in.");
-            loginWrapper.classList.add('hide');
-            })
+            loginWrapper.classList.remove('hide');
+        })
         .catch(function (error) {
             var errorCode = error.code;
             var errorMessage = error.message;
@@ -101,39 +112,28 @@ function login(email, password) {
         });
 }
 
-function register() {
-  var email = document.getElementById("email").value;
-  var password = document.getElementById("password").value;
+function register(email, password) {
 
-  createUserWithEmailAndPassword(auth, email, password)
-    .then(function (user) {
-      alert(user.user.email + " has been registred.");
-      // Send email verification
-      /*currentUser.sendEmailVerification()
-                .then(function() {
-                alert("Verification email sent!");
-                })
-                .catch(function(error) {
-                // Handle errors
-                var errorCode = error.code;
-                var errorMessage = error.message;
-                alert(errorMessage);
-                });
-            })*/
-    })
-    .catch(function (error) {
-      // Handle errors
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      alert(errorMessage);
-    });
+    createUserWithEmailAndPassword(auth, email, password)
+        .then(function (user) {
+            currentUser = user;
+            alert(user.user.email + " has been registred.");
+            // Send email verification
+
+        })
+        .catch(function (error) {
+            // Handle errors
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            alert(errorMessage);
+        });
 }
 
-
+/*
 //DANYS FEATURE
 let danyBtn = document.getElementById("danyBtn");
 let danyh4 = document.getElementById("danyh4");
 
 danyBtn.addEventListener("click", () => {
-  danyh4.classList.toggle("hidden");
-})
+    danyh4.classList.toggle("hidden");
+})*/
