ebc9067590c7a6ced18b4fdae64c2b27d00db3ab
MGritt <112488649+MGritt@users.noreply.github.com>
2023-01-24 08:06:33 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithCredential } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-auth.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithCredential, AuthCredential } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-auth.js";
 // TODO: Add SDKs for Firebase products that you want to use
 import {
     getDatabase, ref, set, onValue, remove, push
